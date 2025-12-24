952d00a4a31ef33299a7c08a1c729dd2f3a79aea
sridharofficial6021-sys <sridharofficial6021@gmail.com>
2025-12-24 21:02:23 +0530

    if else & comparison operator day 03

---
+++ python_problems/python_practics_day_03_.py
@@ -0,0 +1,24 @@
+if(True):
+    print("yes")
+
+else:
+    print("no")
+
+
+print("win"=="win")
+
+
+gt="win"
+if(gt=="win"):
+    print("win the cup")
+
+else:
+    print("lose the cup")
+
+
+maghna=input("died or not:")    
+if(maghna=="died"):
+    print("meet priya")
+
+else:
+    print("marrage maghna")
