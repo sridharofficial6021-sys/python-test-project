c2ce17d39e7452738acb42ecb7be6dfd7baf6bd8
sridharofficial6021-sys <sridharofficial6021@gmail.com>
2025-12-25 22:41:54 +0530

    changes in day 12

---
+++ python_problems/python_practice_day_12_.py
@@ -0,0 +1,30 @@
+def add():
+    print("Addition")
+    a = int(input("Enter the value a: "))
+    b = int(input("Enter the value b: "))
+    print(a + b)
+
+def sub():
+    print("Subtraction")
+    a = int(input("Enter the value a: "))
+    b = int(input("Enter the value b: "))
+    print(a - b)
+
+def mul():
+    print("Multiplication")
+    a = int(input("Enter the value a: "))
+    b = int(input("Enter the value b: "))
+    print(a * b)
+
+def div():
+    print("Division")
+    a = int(input("Enter the value a: "))
+    b = int(input("Enter the value b: "))
+    print(a / b)
+
+add()
+sub()
+mul()
+div()
+
+
