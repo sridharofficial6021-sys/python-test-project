06993e3ca7b19b539cdac2081f77ec7266f56ec2
sridharofficial6021-sys <sridharofficial6021@gmail.com>
2025-12-25 21:53:56 +0530

    python practice while loop

---
+++ python_problems/python_practice_day_10_.py
@@ -0,0 +1,25 @@
+i=1
+while(i<=5):
+    print(i)
+    i=i+1
+
+
+i=1
+while(i<=10):
+    print(i,end="")
+    i=i+1
+
+
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+
+
+i=1
+total=0
+while(i<=5):
+    total=total+1
+    i=i+1
+
+    print("sum:",total)    
