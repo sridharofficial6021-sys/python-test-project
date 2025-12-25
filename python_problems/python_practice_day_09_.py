6959ce89cc8f6c5b12ca9089e3755de940cbae3f
sridharofficial6021-sys <sridharofficial6021@gmail.com>
2025-12-25 15:06:04 +0530

    python practice nested for loop

---
+++ python_problems/python_practice_day_09_.py
@@ -0,0 +1,22 @@
+for i in range (1,6):
+    for j in range (1,3):
+        print(j,"apple")
+
+
+for i in range (1,3):
+    print("weak",i)
+    for j in range (1,4):
+        print("day",j)        
+
+
+for i in range (1,6):
+    print()
+    for j in range(1,i+1):
+        print(j,end="")
+
+
+
+for i in range (1,6):
+    print()
+    for j in range(1,i+1):
+        print("*",end="")        
