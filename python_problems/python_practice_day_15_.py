f3145af01540fc5c120a5b67fb5f4dad0cc0d991
sridharofficial6021-sys <sridharofficial6021@gmail.com>
2025-12-26 18:27:54 +0530

    class & objects

---
+++ python_problems/python_practice_day_15_.py
@@ -0,0 +1,33 @@
+class laptop:
+    price=0
+    proc=""
+    ram=""
+
+hp=laptop()
+dell=laptop()
+apple=laptop()
+
+hp.price=50000
+hp.proc="i5"
+hp.ram="8gb"
+
+dell.price=60000
+dell.proc="i7"
+dell.ram="16gb"
+
+apple.price=100000
+apple.proc="i7"
+apple.ram="32"
+
+
+print(hp.price)
+print(hp.proc)
+print(hp.ram)
+
+print(dell.price)
+print(dell.proc)
+print(dell.ram)
+
+print(apple.price)
+print(apple.proc)
+print(apple.ram)
