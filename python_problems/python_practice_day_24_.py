4f8d39dc0b1dfcd5754a6fd022d2f7bfb068565f
sridharofficial6021-sys <sridharofficial6021@gmail.com>
2025-12-28 18:45:45 +0530

    Encapsulation and Access Modifiers

---
+++ python_problems/python_practice_day_24_.py
@@ -0,0 +1,21 @@
+class company():
+    def __init__(self):
+        self .__companyname="google"
+
+    def companyname(self):
+        print(self.__companyname)
+
+c1=company()
+c1.companyname()     
+
+
+
+class Student:
+    def __init__(self):
+        self.__name = "Ravi"
+
+    def show_name(self):
+        print(self.__name)
+
+s1 = Student()
+s1.show_name()
